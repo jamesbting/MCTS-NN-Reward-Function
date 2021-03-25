d2709bc3267d6e12490bb1a33af682f8673d96c6
James Ting <tingjames2000@outlook.com>
2021-03-25 11:07:45 -0700

    Added grid search functionality

--- src/champion_net.py
+++ src/champion_net.py
@@ -3,13 +3,13 @@ import torch.nn as nn
 
 class ChampionNet(nn.Module):
     #constructor
-    def __init__(self, num_units=120, dropout_rate=0.5):
+    def __init__(self, num_units=120):
         super(ChampionNet, self).__init__() #superclass constructor
         self.fc1 = nn.Linear(154,num_units)
         self.fc2 = nn.Linear(num_units,num_units)
         self.fc3 = nn.Linear(num_units,2)
         self.nonlin = nn.ReLU()
-        self.dropout = nn.Dropout(p=dropout_rate)
+        self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
         x = self.nonlin(self.fc1(x))
