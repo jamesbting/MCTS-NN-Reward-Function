8f7e2222b4fe94400c544af2826ce2276a1d5240
James Ting <tingjames2000@outlook.com>
2021-04-06 18:12:20 -0700

    changed model to be scalar

--- src/champion_net.py
+++ src/champion_net.py
@@ -3,17 +3,18 @@ import torch.nn as nn
 
 class ChampionNet(nn.Module):
     #constructor
-    def __init__(self, num_units=120):
+    def __init__(self, num_units):
         super(ChampionNet, self).__init__() #superclass constructor
         self.fc1 = nn.Linear(154,num_units)
         self.fc2 = nn.Linear(num_units,num_units)
-        self.fc3 = nn.Linear(num_units,2)
+        self.fc3 = nn.Linear(num_units,1)
         self.nonlin = nn.ReLU()
+        self.final_layer_activation = nn.Sigmoid()
         self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
         x = self.nonlin(self.fc1(x))
         x = self.nonlin(self.fc2(x))
         x = self.dropout(x)
-        x = self.fc3(x)
+        x = self.final_layer_activation(self.fc3(x))
         return x
